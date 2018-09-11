888f58ad08780be6dfca270865a4f69d2eb5cf95
larry <ladams1776@yahoo.com>
2018-09-11 07:13:35 -0400

    346. AOP: spring-aop-joinpoints-overview
    347. AOP: spring-aop-reading-joinpoint-code-part-1

--- src/main/java/com/aop/demo/aspects/MyDemoLoggingAspect.java
+++ src/main/java/com/aop/demo/aspects/MyDemoLoggingAspect.java
@@ -1,5 +1,6 @@
 package com.aop.demo.aspects;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -12,7 +13,16 @@ import org.springframework.stereotype.Component;
 public class MyDemoLoggingAspect {
 
     @Before("com.aop.demo.aspects.AopPointcutDeclarations.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
-        System.out.println("\n =====> Executing @Before beforeAddAccountAdvice() advice");
+    public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
+
+        Object[] args = theJoinPoint.getArgs();
+
+
+        System.out.println("\n =====> Executing @Before beforeAddAccountAdvice() advice. " +
+                "From: " + theJoinPoint.getSignature() + " with arguments: ");
+
+        for (Object tempArg: args) {
+            System.out.println(tempArg.toString());
+        }
     }
 }
