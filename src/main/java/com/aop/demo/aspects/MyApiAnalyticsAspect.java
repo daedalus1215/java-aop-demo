8431f65ccd9bbdb8f90a579b05a69141fae4601c
larry <ladams1776@yahoo.com>
2018-09-11 07:06:48 -0400

    345. AOP: spring-aop-pointcut-order-part-3

--- src/main/java/com/aop/demo/aspects/MyApiAnalyticsAspect.java
+++ src/main/java/com/aop/demo/aspects/MyApiAnalyticsAspect.java
@@ -2,15 +2,16 @@ package com.aop.demo.aspects;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 
 @Aspect
 @Component
+@Order(3)
 public class MyApiAnalyticsAspect {
 
-
-    @Before("forDaoPackageNoGetterSetter()")
+    @Before("com.aop.demo.aspects.AopPointcutDeclarations.forDaoPackageNoGetterSetter()")
     public void performApiAnalytics() {
         System.out.println("\n =====> Performing API analytics");
     }
--- src/main/java/com/aop/demo/aspects/MyDemoLoggingAspect.java
+++ src/main/java/com/aop/demo/aspects/MyDemoLoggingAspect.java
@@ -3,13 +3,15 @@ package com.aop.demo.aspects;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(1)
 public class MyDemoLoggingAspect {
-    
-    @Before("forDaoPackageNoGetterSetter()")
+
+    @Before("com.aop.demo.aspects.AopPointcutDeclarations.forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n =====> Executing @Before beforeAddAccountAdvice() advice");
     }
